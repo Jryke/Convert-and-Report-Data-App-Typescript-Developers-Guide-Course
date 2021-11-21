d61cb5a35d9f43327832c0f05b58dc3ad6a09a42
Jesse Rykerson <rykerson@gmail.com>
2021-11-21 15:21:32 -0500

    Oops, My Bad (refactor matchReader declaration)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import { MatchReader } from './MatchReader'
-import { CsvFileReader } from './CsvFileReader'
-import { ConsoleReport } from './ReportTargets/ConsoleReport'
-import { WinsAnalysis } from './Analyzers/WinsAnalysis'
 import { Summary } from './Summary'
-import { HtmlReport } from './ReportTargets/HtmlReport'
 
-// Create an object that satisfies 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv')
-
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader)
-matchReader.load()
-
+const matchReader = MatchReader.fromCsv('football.csv')
 const summary = Summary.winsAnalysisWithHtmlReport('Man United')
 
+matchReader.load()
 summary.buildAndPrintReport(matchReader.matches)
